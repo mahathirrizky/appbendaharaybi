266b911e5e99da878fd5ece415215eda3d3158e5
mahathirrizky <34686493+mahathirrizky@users.noreply.github.com>
2023-06-03 19:53:21 +0800

    get createdat

--- cashflow/entity.go
+++ cashflow/entity.go
@@ -6,8 +6,7 @@ type CashflowTable struct {
 	ID             int
 	Jumlah          int
 	Keterangan          string
-	Jenis   string
-	
+	Jenis   				string
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 }
--- cashflow/formatter.go
+++ cashflow/formatter.go
@@ -1,25 +1,29 @@
 package cashflow
 
+import "time"
+
 type CashflowFormatter struct {
-	ID         int    `json:"id"`
-	Jumlah     int    `json:"jumlah"`
-	Keterangan string `json:"keterangan"`
-	Jenis      string `json:"jenis"`
+	ID         int       `json:"id"`
+	Jumlah     int       `json:"jumlah"`
+	Keterangan string    `json:"keterangan"`
+	Jenis      string    `json:"jenis"`
+	CreatedAt  time.Time `json:"createdAt"`
 }
 
 func FormatCashflow(cashflow CashflowTable) CashflowFormatter {
 	return CashflowFormatter{
-		ID:    cashflow.ID,
-		Jumlah:cashflow.Jumlah,
+		ID:         cashflow.ID,
+		Jumlah:     cashflow.Jumlah,
 		Keterangan: cashflow.Keterangan,
-		Jenis: cashflow.Jenis,
+		Jenis:      cashflow.Jenis,
+		CreatedAt: cashflow.CreatedAt,
 	}
 }
 
-func FormatCashflows(cashflows []CashflowTable) []CashflowFormatter{
+func FormatCashflows(cashflows []CashflowTable) []CashflowFormatter {
 	cashflowsFormatter := []CashflowFormatter{}
-	for _, cashflow := range cashflows{
-		cashflowFormatter:= FormatCashflow(cashflow)
+	for _, cashflow := range cashflows {
+		cashflowFormatter := FormatCashflow(cashflow)
 		cashflowsFormatter = append(cashflowsFormatter, cashflowFormatter)
 	}
 	return cashflowsFormatter
